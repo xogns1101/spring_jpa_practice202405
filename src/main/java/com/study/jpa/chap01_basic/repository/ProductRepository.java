faaa47b0e358276973fc84ede3dd52763e4ff310
dudtjq <dudtjq0415@naver.com>
2024-05-13 15:56:49 +0900

    수업중

--- src/main/java/com/study/jpa/chap01_basic/repository/ProductRepository.java
+++ src/main/java/com/study/jpa/chap01_basic/repository/ProductRepository.java
@@ -1,11 +1,13 @@
 package com.study.jpa.chap01_basic.repository;
 
 import com.study.jpa.chap01_basic.entity.Product;
+import com.study.jpa.chap02_querymethod.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 // Long 프라이머리키의 타입을 알려준거.
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
 
 
 
